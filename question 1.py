c6420738525f0a3d2fce0cf02ae263011429c4ea
palakcse <121235645+palakcse@users.noreply.github.com>
2022-12-22 18:04:51 +0530

    Add files via upload

---
+++ question 1.py
@@ -0,0 +1,6 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=int(input("enter a number:"))
+... b=int(input("enter a number:"))
+... c=int(input("enter a number:"))
+... av=(a+b+c)/3
